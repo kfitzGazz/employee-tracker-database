b81728fe202321cab444a46b8dfea3806c3531b7
Kyleigh Fitzgerald <kyleigh.fitzgerald22@gmail.com>
2023-12-13 14:59:49 -0500

    save queries and seeds. review functionality and update readme next

---
+++ db/queries.sql
@@ -0,0 +1,14 @@
+USE employeetrack_db;
+
+SELECT * FROM department;
+
+SELECT role.id, title, salary, name as department
+FROM role
+LEFT JOIN department ON department.id = role.department_id;
+
+SELECT employee.id, employee.first_name, employee.last_name, title, name as department, salary, 
+CONCAT(bosses.first_name, ' ', bosses.last_name) as manager 
+FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON department.id = role.department_id
+LEFT JOIN employee AS bosses ON employee.manager_id = bosses.id;
--- db/seeds.sql
+++ db/seeds.sql
@@ -8,5 +8,21 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("sales lead", 100000, 1),
 ("salesperson", 80000, 1),
 ("lead engineer", 150000, 2),
-()
+("software Engineer",120000,2),
+("Account Manager",160000,3),
+("Accountant",125000,3),
+("Legal Team Lead",250000,4),
+("Lawyer",190000,4);
+
+INSERT INTO employee (first_name,last_name,role_id)
+VALUES ("Kyleigh","Fitz",1),
+       ("Phil","Loy",2),
+       ("Danny","Wilks",3),
+       ("Quentin","J",4),
+       ("Michael","Jordan",5),
+       ("Steve","Jobs",6),
+       ("Lindsay","Grabuh",7),
+       ("Amy","Poehler",8);
+
+UPDATE employee SET manager_id = 1 WHERE id=2;
 
